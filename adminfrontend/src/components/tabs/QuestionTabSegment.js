209194dafa14eeaebe25bc6cc9695d1786cc9cb8
mehmet demircan <mehmeddemircan@gmail.com>
2023-04-30 21:13:58 +0300

    created question tab segment for survey details

---
+++ adminfrontend/src/components/tabs/QuestionTabSegment.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import SurveyCard from "../card/SurveyCard";
+import { useDispatch, useSelector } from "react-redux";
+import AddQuestionModal from "../modal/question/AddQuestionModal";
+import { useParams } from "react-router-dom";
+import {
+  AllQuestionOfSurvey,
+  CreateQuestion,
+} from "../../redux/actions/QuestionActions";
+import { Card, message } from "antd";
+import QuestionCard from "../card/QuestionCard";
+import {
+  CREATE_QUESTION_RESET,
+  DELETE_QUESTION_RESET,
+  UPDATE_QUESTION_RESET,
+} from "../../redux/constants/QuestionConstants";
+import AddQuestionForm from "../form/AddQuestionForm";
+
+const QuestionTabSegment = () => {
+  const getSingleSurvey = useSelector((state) => state.survey.getSingleSurvey);
+
+  const [showAddQuestionModal, setShowAddQuestionModal] = useState(false);
+
+  const handleShowAddQuestionModal = () => {
+    setShowAddQuestionModal(true);
+  };
+  const handleCloseAddQuestionModal = () => {
+    setShowAddQuestionModal(false);
+  };
+
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const createQuestion = useSelector((state) => state.question.createQuestion);
+  const deleteUpdateQuestion = useSelector(
+    (state) => state.question.deleteUpdateQuestion
+  );
+
+  useEffect(() => {
+    dispatch(AllQuestionOfSurvey(id));
+    if (createQuestion.success) {
+      message.success(createQuestion.message);
+      dispatch({ type: CREATE_QUESTION_RESET });
+    }
+    if (deleteUpdateQuestion.isDeleted) {
+      message.success(deleteUpdateQuestion.message);
+      dispatch({ type: DELETE_QUESTION_RESET });
+    }
+    if (deleteUpdateQuestion.isUpdated) {
+      message.success(deleteUpdateQuestion.message);
+      dispatch({ type: UPDATE_QUESTION_RESET });
+    }
+  }, [
+    dispatch,
+    createQuestion.success,
+    deleteUpdateQuestion.isDeleted,
+    deleteUpdateQuestion.isUpdated,
+  ]);
+  const getAllQuestion = useSelector((state) => state.question.getAllQuestion);
+
+ 
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-end">
+        <button
+          className="btn btn-primary rounded-pill"
+          onClick={handleShowAddQuestionModal}
+        >
+          Soru Ekle
+        </button>
+        <AddQuestionModal
+          showAddQuestionModal={showAddQuestionModal}
+          handleCloseAddQuestionModal={handleCloseAddQuestionModal}
+        />
+      </div>
+      <SurveyCard isDetailsCard={true} survey={getSingleSurvey.survey} />
+
+      <h6>Sorular</h6>
+      <hr />
+      {getAllQuestion.data.questions.map((question) => (
+        <QuestionCard key={question._id} question={question} />
+      ))}
+    </div>
+  );
+};
+
+export default QuestionTabSegment;
