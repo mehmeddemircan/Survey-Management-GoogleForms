e9599d6fb85ade10aa68b7f5fe461821e353a958
mehmet demircan <mehmeddemircan@gmail.com>
2023-04-30 22:06:36 +0300

    add survey favorite , remove , get all  actions

--- adminfrontend/src/redux/actions/UserActions.js
+++ adminfrontend/src/redux/actions/UserActions.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-import { DELETE_USER_FAIL, DELETE_USER_REQUEST, DELETE_USER_SUCCESS, GET_ALL_USER_FAIL, GET_ALL_USER_REQUEST, GET_ALL_USER_SUCCESS } from "../constants/UserConstants";
+import { ADD_SURVEY_TO_FAVORITE_FAIL, ADD_SURVEY_TO_FAVORITE_REQUEST, ADD_SURVEY_TO_FAVORITE_SUCCESS, DELETE_USER_FAIL, DELETE_USER_REQUEST, DELETE_USER_SUCCESS, GET_ALL_FAVORITES_FAIL, GET_ALL_FAVORITES_REQUEST, GET_ALL_FAVORITES_SUCCESS, GET_ALL_USER_FAIL, GET_ALL_USER_REQUEST, GET_ALL_USER_SUCCESS, REMOVE_SURVEY_FROM_FAVORITE_FAIL, REMOVE_SURVEY_FROM_FAVORITE_REQUEST, REMOVE_SURVEY_FROM_FAVORITE_SUCCESS } from "../constants/UserConstants";
 
 // getirme işlemi
 export const AllUser = (limit,page) => async (dispatch) => {
@@ -41,6 +41,73 @@ export const DeleteUser = (userId) => async (dispatch) => {
     } catch (error) {
       dispatch({
         type: DELETE_USER_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+
+export const GetSurveyFavorites = (userId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_ALL_FAVORITES_REQUEST,
+      });
+  
+      const { data } = await axios.get(
+        `http://localhost:5000/api/users/${userId}/favorites`
+      );
+  
+      dispatch({
+        type: GET_ALL_FAVORITES_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_ALL_FAVORITES_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+  export const AddSurveyToFavorite = (userId,surveyId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: ADD_SURVEY_TO_FAVORITE_REQUEST,
+      });
+  
+      const { data } = await axios.put(
+        `http://localhost:5000/api/users/${userId}/surveys/${surveyId}/add-favorite`
+      );
+  
+      dispatch({
+        type: ADD_SURVEY_TO_FAVORITE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ADD_SURVEY_TO_FAVORITE_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+  export const RemoveSurveyFromFavorite = (userId,surveyId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: REMOVE_SURVEY_FROM_FAVORITE_REQUEST,
+      });
+  
+      const { data } = await axios.put(
+        `http://localhost:5000/api/users/${userId}/surveys/${surveyId}/remove-favorite`
+      );
+  
+      dispatch({
+        type: REMOVE_SURVEY_FROM_FAVORITE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: REMOVE_SURVEY_FROM_FAVORITE_FAIL,
         error: error.response,
       });
     }
