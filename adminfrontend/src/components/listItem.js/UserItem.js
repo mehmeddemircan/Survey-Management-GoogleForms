373a88d8de2b1e094ea96c3b2f41baf8b0f3016e
mehmet demircan <mehmeddemircan@gmail.com>
2023-04-30 21:08:40 +0300

    changed user avater in userItem

--- adminfrontend/src/components/listItem.js/UserItem.js
+++ adminfrontend/src/components/listItem.js/UserItem.js
@@ -5,36 +5,33 @@ import { useDispatch } from "react-redux";
 import { DeleteUser } from "../../redux/actions/UserActions";
 
 const UserItem = ({ user }) => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleDeleteUser = (id) => {
-    dispatch(DeleteUser(id))
-  }
-
+    dispatch(DeleteUser(id));
+  };
 
   return (
     <List.Item
       actions={[
         <a key="list-loadmore-edit">edit</a>,
         <a key="list-loadmore-more">more</a>,
-        <i class="fa-sharp fa-solid fa-trash text-danger" onClick={() => handleDeleteUser(user._id)} ></i>,
+        <i
+          class="fa-sharp fa-solid fa-trash text-danger"
+          onClick={() => handleDeleteUser(user._id)}
+        ></i>,
       ]}
     >
       <List.Item.Meta
         avatar={
-          user.avatar ? 
-          <Image
-          className="img-fluid"
-          width={40}
-          src={
-            user.avatar
-              ? user.avatar.url
-              : "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-          }
-        /> : 
-          <Avatar src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg" size={"large"} />
-         
+          user.avatar ? (
+            <Image className="img-fluid" width={40} src={user.avatar.url} />
+          ) : (
+            <Avatar
+              src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+              size={"large"}
+            />
+          )
         }
         title={<a>{user.firstname}</a>}
         description="Ant Design, a design language for background applications, is refined by Ant UED Team"
