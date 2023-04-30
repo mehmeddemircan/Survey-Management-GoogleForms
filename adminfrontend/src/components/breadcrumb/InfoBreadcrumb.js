b923bb8432c213f085e730364815e693c028ac27
mehmet demircan <mehmeddemircan@gmail.com>
2023-04-30 21:03:24 +0300

    generated generic info breadcrumb

--- adminfrontend/src/components/breadcrumb/InfoBreadcrumb.js
+++ adminfrontend/src/components/breadcrumb/InfoBreadcrumb.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const InfoBreadcrumb = ({separator = '/' , items =[]}) => {
   return (
-    <Breadcrumb separator={separator} className='mt-4'>
+    <Breadcrumb separator={separator} >
     {items.map((item, index) => (
       <Breadcrumb.Item key={index}>{item.title}</Breadcrumb.Item>
     ))}
