6d46806e97f25dc36566e17e6d8d7ad3d42d4102
mehmet demircan <mehmeddemircan@gmail.com>
2023-04-30 21:15:26 +0300

    created question actions

---
+++ adminfrontend/src/redux/actions/QuestionActions.js
@@ -0,0 +1,110 @@
+import axios from "axios";
+import {
+  ADD_OPTION,
+  CREATE_QUESTION_FAIL,
+  CREATE_QUESTION_REQUEST,
+  CREATE_QUESTION_SUCCESS,
+  DELETE_QUESTION_FAIL,
+  DELETE_QUESTION_REQUEST,
+  DELETE_QUESTION_SUCCESS,
+  GET_ALL_QUESTION_FAIL,
+  GET_ALL_QUESTION_REQUEST,
+  GET_ALL_QUESTION_SUCCESS,
+  UPDATE_QUESTION_FAIL,
+  UPDATE_QUESTION_REQUEST,
+  UPDATE_QUESTION_SUCCESS,
+} from "../constants/QuestionConstants";
+
+export const AllQuestionOfSurvey = (surveyId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_ALL_QUESTION_REQUEST,
+    });
+
+    const { data } = await axios.get(
+      `http://localhost:5000/api/surveys/${surveyId}/questions`
+    );
+
+    dispatch({
+      type: GET_ALL_QUESTION_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ALL_QUESTION_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+export const CreateQuestion = (question) => async (dispatch) => {
+  try {
+    dispatch({
+      type: CREATE_QUESTION_REQUEST,
+    });
+
+    const { data } = await axios.post(
+      `http://localhost:5000/api/create-question`,
+      question
+    );
+
+    dispatch({
+      type: CREATE_QUESTION_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: CREATE_QUESTION_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+export const DeleteQuestion = (surveyId, questionId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: DELETE_QUESTION_REQUEST,
+    });
+
+    const { data } = await axios.delete(
+      `http://localhost:5000/api/surveys/${surveyId}/questions/${questionId}/delete`
+    );
+
+    dispatch({
+      type: DELETE_QUESTION_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: DELETE_QUESTION_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+
+export const UpdateQuestion = (questionId, question) => async (dispatch) => {
+  try {
+    dispatch({
+      type: UPDATE_QUESTION_REQUEST,
+    });
+
+    const { data } = await axios.put(
+      `http://localhost:5000/api/questions/${questionId}/update`,question
+    );
+
+    dispatch({
+      type: UPDATE_QUESTION_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_QUESTION_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+
+
+
