d1e207ffce2cc6c7b1fdd31251b798bf3f9a5840
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-01 11:00:34 +0300

    added loading spinner to list

--- adminfrontend/src/components/list/SurveyList.js
+++ adminfrontend/src/components/list/SurveyList.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 import SurveyCard from "../card/SurveyCard";
-import { List } from "antd";
+import { Empty, List } from "antd";
 import { useSelector } from "react-redux";
+import LoadingSpinner from "../spinner/LoadingSpinner";
+import EmptyComponent from "../empty/EmptyComponent";
 
 const SurveyList = ({ title }) => {
   const getAllSurvey = useSelector((state) => state.survey.getAllSurvey);
@@ -12,14 +14,16 @@ const SurveyList = ({ title }) => {
       <List className="my-4" itemLayout="horizontal">
         {title ? (
           <>
-            {searchSurveys.surveys.map((survey) => (
+            {searchSurveys.loading ? <LoadingSpinner /> : searchSurveys.surveys.length === 0 ? <EmptyComponent />  :  searchSurveys.surveys.map((survey) => (
               <SurveyCard key={survey._id} survey={survey} />
             ))}
           </>
         ) : (
-          getAllSurvey.surveys.map((survey) => (
+         <>
+          {getAllSurvey.loading ? <LoadingSpinner /> : getAllSurvey.surveys.length === 0 ? <EmptyComponent /> : getAllSurvey.surveys.map((survey) => (
             <SurveyCard key={survey._id} survey={survey} />
-          ))
+          ))}
+         </>
         )}
       </List>
     </Fragment>
