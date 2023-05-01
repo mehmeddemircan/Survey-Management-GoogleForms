6edd04cc930ef3345020f417ba056acf21e6f024
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-01 11:01:39 +0300

    get profile update profile actions created

--- adminfrontend/src/redux/actions/UserActions.js
+++ adminfrontend/src/redux/actions/UserActions.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-import { ADD_SURVEY_TO_FAVORITE_FAIL, ADD_SURVEY_TO_FAVORITE_REQUEST, ADD_SURVEY_TO_FAVORITE_SUCCESS, DELETE_USER_FAIL, DELETE_USER_REQUEST, DELETE_USER_SUCCESS, GET_ALL_FAVORITES_FAIL, GET_ALL_FAVORITES_REQUEST, GET_ALL_FAVORITES_SUCCESS, GET_ALL_USER_FAIL, GET_ALL_USER_REQUEST, GET_ALL_USER_SUCCESS, REMOVE_SURVEY_FROM_FAVORITE_FAIL, REMOVE_SURVEY_FROM_FAVORITE_REQUEST, REMOVE_SURVEY_FROM_FAVORITE_SUCCESS } from "../constants/UserConstants";
+import { ADD_SURVEY_TO_FAVORITE_FAIL, ADD_SURVEY_TO_FAVORITE_REQUEST, ADD_SURVEY_TO_FAVORITE_SUCCESS, DELETE_USER_FAIL, DELETE_USER_REQUEST, DELETE_USER_SUCCESS, GET_ALL_FAVORITES_FAIL, GET_ALL_FAVORITES_REQUEST, GET_ALL_FAVORITES_SUCCESS, GET_ALL_USER_FAIL, GET_ALL_USER_REQUEST, GET_ALL_USER_SUCCESS, GET_PROFILE_FAIL, GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, REMOVE_SURVEY_FROM_FAVORITE_FAIL, REMOVE_SURVEY_FROM_FAVORITE_REQUEST, REMOVE_SURVEY_FROM_FAVORITE_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS } from "../constants/UserConstants";
 
 // getirme işlemi
 export const AllUser = (limit,page) => async (dispatch) => {
@@ -112,3 +112,68 @@ export const GetSurveyFavorites = (userId) => async (dispatch) => {
       });
     }
   };
+
+
+  export const GetProfile = () => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_PROFILE_REQUEST,
+      });
+      const token = localStorage.getItem('token');
+    
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+      };
+ 
+      const { data } = await axios.get(
+        `http://localhost:5000/api/profile/me`,config
+      );
+  
+      dispatch({
+        type: GET_PROFILE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_PROFILE_FAIL,
+        error: error.response,
+      });
+    }
+  };
+  
+
+  export const UpdateProfile = (user) => async (dispatch) => {
+    try {
+      dispatch({
+        type: UPDATE_PROFILE_REQUEST,
+      });
+      const token = localStorage.getItem('token');
+    
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+      };
+ 
+      const { data } = await axios.put(
+        `http://localhost:5000/api/profile/update`,user,config
+      );
+      localStorage.setItem("user", JSON.stringify(data.user))
+      localStorage.setItem("token",data.token)
+      
+      dispatch({
+        type: UPDATE_PROFILE_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_PROFILE_FAIL,
+        error: error.response,
+      });
+    }
+  };
+  
