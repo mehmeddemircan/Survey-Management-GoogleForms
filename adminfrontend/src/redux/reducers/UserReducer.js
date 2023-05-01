d991947bfed3313a6f7e229d1f69c981be2a158d
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-01 11:02:40 +0300

    get profile , update profile reducer created

--- adminfrontend/src/redux/reducers/UserReducer.js
+++ adminfrontend/src/redux/reducers/UserReducer.js
@@ -14,10 +14,17 @@ import {
   GET_ALL_USER_FAIL,
   GET_ALL_USER_REQUEST,
   GET_ALL_USER_SUCCESS,
+  GET_PROFILE_FAIL,
+  GET_PROFILE_REQUEST,
+  GET_PROFILE_SUCCESS,
   REMOVE_SURVEY_FROM_FAVORITE_FAIL,
   REMOVE_SURVEY_FROM_FAVORITE_REQUEST,
   REMOVE_SURVEY_FROM_FAVORITE_RESET,
   REMOVE_SURVEY_FROM_FAVORITE_SUCCESS,
+  UPDATE_PROFILE_FAIL,
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_RESET,
+  UPDATE_PROFILE_SUCCESS,
 } from "../constants/UserConstants";
 
 const GET_ALL_USER_INITIAL_STATE = {
@@ -52,6 +59,36 @@ export const getAllUserReducer = (
       return state;
   }
 };
+
+export const getProfileReducer = (
+  state = {user: {}},
+  action
+) => {
+  switch (action.type) {
+    case GET_PROFILE_REQUEST:
+      return { ...state, loading: true };
+
+    case GET_PROFILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        
+        user: action.payload.user,
+      };
+
+    case GET_PROFILE_FAIL:
+      return {
+        ...state,
+        loading: false,
+        success: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 
 const DELETE_UPDATE_USER_INITIAL_STATE = {
   message: null,
@@ -177,11 +214,39 @@ export const addRemoveFavoriteReducer = (
   }
 };
 
+
+export const updateProfileReducer = (state = { token : "", user : {} }, action) => {
+  switch (action.type) {
+    case UPDATE_PROFILE_REQUEST:
+      return { ...state, loading: true };
+    case UPDATE_PROFILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        token : action.payload.token,
+        user: action.payload.user,
+      };
+
+    case UPDATE_PROFILE_FAIL:
+      return { ...state, loading: false, error: action.payload };
+
+      case UPDATE_PROFILE_RESET:
+        return { ...state,success: false, token : "" , user : {} };
+    default:
+      return state;
+  }
+};
+
+
 const userReducer = combineReducers({
   getAllUser: getAllUserReducer,
   deleteUpdateUser: deleteUpdateUserReducer,
   getUserFavorites: getUserFavoritesReducer,
-  addRemoveFavorite : addRemoveFavoriteReducer
+  addRemoveFavorite : addRemoveFavoriteReducer,
+  getProfile : getProfileReducer,
+  updateProfile : updateProfileReducer
+
 });
 
 export default userReducer;
