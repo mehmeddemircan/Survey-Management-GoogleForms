a703def8db6bb0ebfe108bb1fb13c145f5528160
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-03 09:45:48 +0300

    created circular progress

---
+++ adminfrontend/src/components/circularProgress/CircularProgress.js
@@ -0,0 +1,71 @@
+import React, { Fragment } from "react";
+import { Progress } from "antd";
+import InfoTooltip from "../tooltip/InfoTooltip";
+
+const CircularProgress = ({ question }) => {
+  const totalOptions = question.options.length;
+  let totalResponses = 0;
+  let optionPercentages = {};
+
+  const responsesCount = question.responses.length;
+  const optionsCount = question.options.length;
+
+  if (optionsCount === 0) {
+    // If the question has no options, assume it's a short answer question
+    totalResponses += 1;
+  } else {
+    //   Calculate the percentage of responses for each option
+    question.options.forEach((option, index) => {
+      const optionResponses = question.responses.filter(
+        (response) => response === option
+      ).length;
+      const optionPercentage = (optionResponses / responsesCount) * 100;
+      totalResponses += optionPercentage;
+      optionPercentages[option] = optionPercentages[option] || 0;
+      optionPercentages[option] += optionPercentage;
+    });
+  }
+  //   });
+
+  const percentage = Math.round(totalResponses / totalOptions);
+  const colors = ["#f5222d", "#fa8c16", "#1890ff", "#52c41a", "#722ed1"];
+
+  const optionsWithColor = Object.entries(optionPercentages)
+    .sort(([, a], [, b]) => b - a) // Sort options by percentage in descending order
+    .map(([option, percentage], index) => ({
+      option,
+      percentage,
+      color: colors[index % colors.length], // Cycle through the available colors
+    }));
+
+  return (
+    <Fragment>
+      <Progress type="circle" percent={percentage} />
+      <div style={{ marginLeft: 32 }}>
+        {optionsWithColor.map(({ option, percentage, color }) => (
+            <InfoTooltip >
+                 <div
+            key={option}
+            style={{ display: "flex", alignItems: "center", marginBottom: 8 }}
+          >
+            <div
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: color,
+                marginRight: 8,
+              }}
+            />
+            <div>{option}</div>
+            <div style={{ marginLeft: "auto", fontWeight: "bold" }}>
+              {Math.round(percentage)}%
+            </div>
+          </div>
+            </InfoTooltip>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+export default CircularProgress;
