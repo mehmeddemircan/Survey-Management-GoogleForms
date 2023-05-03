f004b519d4575c1edb5997b111343b463330838b
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-03 10:07:55 +0300

    created redux store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import surveyReducer from "./reducer/SurveyReducer";
+
+
+const rootReducer = combineReducers({
+    survey : surveyReducer
+})
+
+const initialState = {}
+
+const middleware = [thunk];
+
+
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
