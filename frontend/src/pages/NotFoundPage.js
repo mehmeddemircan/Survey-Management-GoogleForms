7294baeafe78ec778f363dbdcc63a4ea3acba6d2
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-03 10:07:11 +0300

    created not found page

---
+++ frontend/src/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Result } from 'antd';
+import InfoResult from '../components/result/InfoResult';
+import NotFoundResult from '../components/result/NotFoundResult';
+const NotFoundPage = () => {
+  return (
+
+        <NotFoundResult />
+       
+  )
+}
+
+export default NotFoundPage
