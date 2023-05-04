25ad2a808174b9a1666fc8c135fffc60a0fdfe0f
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-04 18:30:20 +0300

    added submit , single preview survey reducers

--- adminfrontend/src/redux/reducers/SurveyReducer.js
+++ adminfrontend/src/redux/reducers/SurveyReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { CREATE_SURVEY_FAIL, CREATE_SURVEY_REQUEST, CREATE_SURVEY_RESET, CREATE_SURVEY_SUCCESS, DELETE_SURVEY_FAIL, DELETE_SURVEY_REQUEST, DELETE_SURVEY_RESET, DELETE_SURVEY_SUCCESS, GET_ALL_SURVEY_FAIL, GET_ALL_SURVEY_REQUEST, GET_ALL_SURVEY_SUCCESS, GET_SINGLE_SURVEY_FAIL, GET_SINGLE_SURVEY_REQUEST, GET_SINGLE_SURVEY_SUCCESS, SEARCH_SURVEYS_FAIL, SEARCH_SURVEYS_REQUEST, SEARCH_SURVEYS_RESET, SEARCH_SURVEYS_SUCCESS, UPDATE_SURVEY_FAIL, UPDATE_SURVEY_REQUEST, UPDATE_SURVEY_RESET, UPDATE_SURVEY_SUCCESS } from "../constants/SurveyConstants";
+import { CREATE_SURVEY_FAIL, CREATE_SURVEY_REQUEST, CREATE_SURVEY_RESET, CREATE_SURVEY_SUCCESS, DELETE_SURVEY_FAIL, DELETE_SURVEY_REQUEST, DELETE_SURVEY_RESET, DELETE_SURVEY_SUCCESS, GET_ALL_SURVEY_FAIL, GET_ALL_SURVEY_REQUEST, GET_ALL_SURVEY_SUCCESS, GET_SINGLE_SURVEY_FAIL, GET_SINGLE_SURVEY_PREVIEW_FAIL, GET_SINGLE_SURVEY_PREVIEW_REQUEST, GET_SINGLE_SURVEY_PREVIEW_SUCCESS, GET_SINGLE_SURVEY_REQUEST, GET_SINGLE_SURVEY_SUCCESS, SEARCH_SURVEYS_FAIL, SEARCH_SURVEYS_REQUEST, SEARCH_SURVEYS_RESET, SEARCH_SURVEYS_SUCCESS, SEND_SURVEY_TO_EMAIL_FAIL, SEND_SURVEY_TO_EMAIL_REQUEST, SEND_SURVEY_TO_EMAIL_RESET, SEND_SURVEY_TO_EMAIL_SUCCESS, SUBMIT_SURVEY_FAIL, SUBMIT_SURVEY_REQUEST, SUBMIT_SURVEY_SUCCESS, UPDATE_SURVEY_FAIL, UPDATE_SURVEY_REQUEST, UPDATE_SURVEY_RESET, UPDATE_SURVEY_SUCCESS } from "../constants/SurveyConstants";
 
 
 const GET_ALL_SURVEY_INITIAL_STATE = {
@@ -191,14 +191,111 @@ const SEARCH_SURVEYS_INITIAL_STATE = {surveys : [] , message : null}
   };
 
 
+  const GET_SINGLE_SURVEY_PREVIEW_INITIAL_STATE = {
+    survey : {},
+ 
+  };
+  export const getSingleSurveyPreviewReducer = (
+    state = GET_SINGLE_SURVEY_PREVIEW_INITIAL_STATE,
+    action
+  ) => {
+    switch (action.type) {
+      case GET_SINGLE_SURVEY_PREVIEW_REQUEST:
+        return { ...state, loading: true };
+  
+      case GET_SINGLE_SURVEY_PREVIEW_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+          survey: action.payload.data,
+        };
+  
+      case GET_SINGLE_SURVEY_PREVIEW_FAIL:
+        return {
+          ...state,
+          loading: false,
+          success: false,
+          error: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+  const SUBMIT_SURVEY_INITIAL_STATE = {
+    message : null
+ 
+  };
+  export const submitSurveyReducer = (
+    state = SUBMIT_SURVEY_INITIAL_STATE,
+    action
+  ) => {
+    switch (action.type) {
+      case SUBMIT_SURVEY_REQUEST:
+        return { ...state, loading: true };
+  
+      case SUBMIT_SURVEY_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+          message: action.payload.message,
+        };
+  
+      case SUBMIT_SURVEY_FAIL:
+        return {
+          ...state,
+          loading: false,
+          success: false,
+          error: action.payload.error,
+        };
+      default:
+        return state;
+    }
+  };
 
 
+  export const sendSurveyToEmailReducer = (
+    state = {message: null},
+    action
+  ) => {
+    switch (action.type) {
+      case SEND_SURVEY_TO_EMAIL_REQUEST:
+        return { ...state, loading: true };
+  
+      case SEND_SURVEY_TO_EMAIL_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+          message: action.payload.message,
+        };
+  
+      case SEND_SURVEY_TO_EMAIL_FAIL:
+        return {
+          ...state,
+          loading: false,
+          success: false,
+          error: action.payload.error,
+        };
+      case SEND_SURVEY_TO_EMAIL_RESET:
+          return {
+            ...state,
+            success: false
+          };
+      default:
+        return state;
+    }
+  };
 const surveyReducer = combineReducers({
     getAllSurvey : getAllSurveyReducer,
     createSurvey : createSurveyReducer,
     deleteUpdateSurvey : deleteUpdateSurveyReducer,
     searchSurveys: searchSurveysReducer,
-    getSingleSurvey : getSingleSurveyReducer
+    getSingleSurvey : getSingleSurveyReducer,
+    getSingleSurveyPreview : getSingleSurveyPreviewReducer,
+    submitSurvey : submitSurveyReducer,
+    sendSurveyToEmail : sendSurveyToEmailReducer
 })
 
 export default surveyReducer
