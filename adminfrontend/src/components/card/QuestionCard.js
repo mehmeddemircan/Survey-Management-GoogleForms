633f9e08f11e6ed0bd3c6bc3a7b5a847332bb98e
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-04 18:20:19 +0300

    added popconfirm to delete question

--- adminfrontend/src/components/card/QuestionCard.js
+++ adminfrontend/src/components/card/QuestionCard.js
@@ -7,111 +7,140 @@ import { DeleteQuestion } from "../../redux/actions/QuestionActions";
 import { useParams } from "react-router-dom";
 import EditQuestionModal from "../modal/question/EditQuestionModal";
 import CircularProgress from "../circularProgress/CircularProgress";
+import InfoPopconfirm from "../popconfirm/InfoPopconfirm";
 
-const QuestionCard = ({isResponseCard,responses, isDrawerCard,question }) => {
+const QuestionCard = ({
+  isResponseCard,
+  responses,
+  isDrawerCard,
+  question,
+  
+}) => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
 
-    const dispatch = useDispatch()
-    const {id} = useParams()
+  const handleDeleteQuestion = (questionId) => {
+    dispatch(DeleteQuestion(id, questionId));
+  };
 
-    const handleDeleteQuestion = (questionId) => {
-        dispatch(DeleteQuestion(id,questionId))
-    } 
+  const [showEditQuestionModal, setShowEditQuestionModal] = useState(false);
 
-    const [showEditQuestionModal, setShowEditQuestionModal] = useState(false)
+  const handleShowEditQuestionModal = () => {
+    setShowEditQuestionModal(true);
+  };
 
-    const handleShowEditQuestionModal = () => {
-      setShowEditQuestionModal(true)
-    }
-
-    const handleCloseEditQuestionModal = () => {
-      setShowEditQuestionModal(false)
-    }
+  const handleCloseEditQuestionModal = () => {
+    setShowEditQuestionModal(false);
+  };
 
   return (
     <Fragment>
-
       <Card
         className="my-2"
         hoverable
-        title={<><h6 className="d-inline-block">{question.questionText}</h6>{question.isRequired ? <i class="fa-sharp fa-solid fa-star-of-life mx-2 "style={{fontSize:'10px',color : 'red'}}></i> : null}</>}
+        title={
+          <>
+            <h6 className="d-inline-block">{question.questionText}</h6>
+            {question.isRequired ? (
+              <i
+                class="fa-sharp fa-solid fa-star-of-life mx-2 "
+                style={{ fontSize: "10px", color: "red" }}
+              ></i>
+            ) : null}
+          </>
+        }
         bordered={true}
         extra={
-          
           isDrawerCard || isResponseCard ? null : (
             <>
-            <button className="btn btn-light mx-2" onClick={handleShowEditQuestionModal}>Düzenle</button>
-
-            <EditQuestionModal 
-              question={question}
-              showEditQuestionModal={showEditQuestionModal}
-              handleCloseEditQuestionModal={handleCloseEditQuestionModal}
-            />
-
-            <InfoTooltip text="Soruyu Sil">
-              <button className="btn btn-light" onClick={() => handleDeleteQuestion(question._id)}>
-                <i class="fa-solid fa-trash"></i>
+              <button
+                className="btn btn-light mx-2"
+                onClick={handleShowEditQuestionModal}
+              >
+                Düzenle
               </button>
-            </InfoTooltip>
-          </>
+
+              <EditQuestionModal
+                question={question}
+                showEditQuestionModal={showEditQuestionModal}
+                handleCloseEditQuestionModal={handleCloseEditQuestionModal}
+              />
+
+              <InfoTooltip text="Soruyu Sil">
+                <InfoPopconfirm
+                  title="Soruyu Silmek"
+                  description={`${question.questionText} silmek istediğiniz den emin misiniz ? `}
+                  confirm={() => handleDeleteQuestion(question._id)}
+                >
+                  <button className="btn btn-light">
+                    {" "}
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </InfoPopconfirm>
+              </InfoTooltip>
+            </>
           )
         }
       >
         {isResponseCard ? (
           <div className="d-flex justify-content-between">
-              <div className="d-inline-flex flex-column">
+            <div className="d-inline-flex flex-column">
               {responses.map((response) => (
-              <div>{response}</div>
-            ))}
-              </div>
-              
-            {question.questionType === "Çoktan Seçmeli" &&   <CircularProgress question={question} options={question.options} responses={responses} />}
+                <div>{response}</div>
+              ))}
+            </div>
+
+            {question.questionType === "Çoktan Seçmeli" && (
+              <CircularProgress
+                question={question}
+                options={question.options}
+                responses={responses}
+              />
+            )}
           </div>
         ) : (
           <>
             {question.questionType === "Kısa Yanıt" && (
-          <Input
-            disabled={true}
-            className="w-25"
-            placeholder="Kısa Yanıt"
-            bordered={false}
-            style={{ borderBottom: "1px solid rgb(221,221,221)" }}
-          />
-        )}
+              <Input
+                disabled={true}
+                className="w-25"
+                placeholder="Kısa Yanıt"
+                bordered={false}
+                style={{ borderBottom: "1px solid rgb(221,221,221)" }}
+              />
+            )}
 
-        {question.questionType === "Uzun Yanıt" && (
-          <TextArea
-            disabled={true}
-            className="w-25"
-            rows={5}
-            maxLength={200}
-            placeholder="Uzun Yanıt"
-            bordered={false}
-            style={{ borderBottom: "1px solid rgb(221,221,221)" }}
-          />
-        )}
+            {question.questionType === "Uzun Yanıt" && (
+              <TextArea
+                disabled={true}
+                className="w-25"
+                rows={5}
+                maxLength={250}
+                placeholder="Uzun Yanıt"
+                bordered={false}
+                style={{ borderBottom: "1px solid rgb(221,221,221)" }}
+              />
+            )}
 
-        {question.questionType === "Çoktan Seçmeli" && (
-         <>
-          <div className="d-flex flex-column">
-            {question.options.map((option) => (
-              <div className="d-inline-flex">
-                <Radio disabled={true} />
-                <Input
-                  disabled={true}
-                  placeholder="Seçenek Giriniz"
-                  bordered={false}
-                  defaultValue={option}
-                />
-              </div>
-            ))}
-          </div>
-        
-         </>
-        )}
+            {question.questionType === "Çoktan Seçmeli" && (
+              <>
+                <div className="d-flex flex-column">
+                  {question.options.map((option) => (
+                    <div className="d-inline-flex">
+                      <Radio disabled={true} />
+                      <Input
+                        disabled={true}
+                        placeholder="Seçenek Giriniz"
+                        bordered={false}
+                        defaultValue={option}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
           </>
         )}
-
-
       </Card>
     </Fragment>
   );
