85264045b486c51ad9bad6c646664a250ed06898
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-07 14:11:00 +0300

    added home page to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
 import SurveyDetailsPage from './pages/SurveyDetailsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
         <Router>
           <Routes>
-            <Route index path='/anketler/:id' element={<SurveyDetailsPage />} />
+            <Route index path='/' element={<HomePage />} />
+            <Route  path='/anketler/:id' element={<SurveyDetailsPage />} />
             <Route  path='*' element={<NotFoundPage />} />
           </Routes>
         </Router>
