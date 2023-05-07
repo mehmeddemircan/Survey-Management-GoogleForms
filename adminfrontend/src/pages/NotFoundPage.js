8c677bad69f9d160812b8c57393486af63cd78be
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-07 13:22:55 +0300

    added meta title to found page

--- adminfrontend/src/pages/NotFoundPage.js
+++ adminfrontend/src/pages/NotFoundPage.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Button, Result } from 'antd';
 import MainLayout from '../components/layout/MainLayout';
+import MetaTitle from '../meta/MetaTitle';
 const NotFoundPage = () => {
   return (
         <MainLayout>
+                <MetaTitle title="Akınsoft Sayfa Bulunamadı" name="sayfabulunamadı" content="Sayfa bulunamadı" />
             <Result
     status="404"
     title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary" href='/'>Back Home</Button>}
+    subTitle="Üzgünüz Aradığın sayfa bulunamadı... "
+    extra={<Button type="primary" href='/'>Anasayfaya Dön</Button>}
   />
         </MainLayout>
   )
