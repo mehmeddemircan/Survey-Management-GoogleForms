dd438559daaefa7acfff892d8b3fff70b266a990
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-07 13:20:43 +0300

    when passed response tab , run useEffect response

--- adminfrontend/src/components/tabs/ResponseTabSegment.js
+++ adminfrontend/src/components/tabs/ResponseTabSegment.js
@@ -1,35 +1,48 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import LoadingSpinner from '../spinner/LoadingSpinner'
-import SurveyCard from '../card/SurveyCard'
-import EmptyComponent from '../empty/EmptyComponent'
-import QuestionCard from '../card/QuestionCard'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingSpinner from "../spinner/LoadingSpinner";
+import SurveyCard from "../card/SurveyCard";
+import EmptyComponent from "../empty/EmptyComponent";
+import QuestionCard from "../card/QuestionCard";
+import { AllQuestionOfSurvey } from "../../redux/actions/QuestionActions";
+import { useParams } from "react-router-dom";
 
 const ResponseTabSegment = () => {
+  const getSingleSurvey = useSelector((state) => state.survey.getSingleSurvey);
+  const getAllQuestion = useSelector((state) => state.question.getAllQuestion);
 
-    const getSingleSurvey = useSelector((state) => state.survey.getSingleSurvey)
-    const getAllQuestion = useSelector((state) => state.question.getAllQuestion)
-
-  
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(AllQuestionOfSurvey(id));
+  }, [dispatch, id]);
 
   return (
-   <div className='container'>
-    {getSingleSurvey.loading ? <LoadingSpinner />  : <SurveyCard isDetailsCard={true} survey={getSingleSurvey.survey} />}
+    <div className="container">
+      {getSingleSurvey.loading ? (
+        <LoadingSpinner />
+      ) : (
+        <SurveyCard isDetailsCard={true} survey={getSingleSurvey.survey} />
+      )}
 
-<h6>Sorular</h6>
-<hr />
-{getAllQuestion.loading ? (
-  <LoadingSpinner />
-) : getAllQuestion.data.questions.length === 0 ? (
-  <EmptyComponent />
-) : (
-  getAllQuestion.data.questions.map((question) => (
-    <QuestionCard responses={question.responses} isResponseCard={true} key={question._id} question={question} />
-  ))
-)}
+      <h6>Sorular</h6>
+      <hr />
+      {getAllQuestion.loading ? (
+        <LoadingSpinner />
+      ) : getAllQuestion.data.questions.length === 0 ? (
+        <EmptyComponent />
+      ) : (
+        getAllQuestion.data.questions.map((question) => (
+          <QuestionCard
+            responses={question.responses}
+            isResponseCard={true}
+            key={question._id}
+            question={question}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
-   </div>
-  )
-}
-
-export default ResponseTabSegment
+export default ResponseTabSegment;
