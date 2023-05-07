0794e0640d222c7925ea462ffd47c23051888cab
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-07 13:24:13 +0300

    added favorite from survey details page& metatitle

--- adminfrontend/src/pages/SurveyDetailsPage.js
+++ adminfrontend/src/pages/SurveyDetailsPage.js
@@ -7,11 +7,15 @@ import { GetSingleSurvey } from '../redux/actions/SurveyActions'
 import SurveyCard from '../components/card/SurveyCard'
 import { message } from 'antd'
 import { UPDATE_SURVEY_RESET } from '../redux/constants/SurveyConstants'
+import { GetSurveyFavorites } from '../redux/actions/UserActions'
+import { ADD_SURVEY_TO_FAVORITE_RESET, REMOVE_SURVEY_FROM_FAVORITE_RESET } from '../redux/constants/UserConstants'
 
 const SurveyDetailsPage = () => {
 
     const dispatch = useDispatch()
     const deleteUpdateSurvey = useSelector((state) => state.survey.deleteUpdateSurvey)
+    const auth = useSelector((state) => state.auth)
+    const addRemoveFavorite = useSelector((state) => state.user.addRemoveFavorite)
     const {id} = useParams()
 
     useEffect(() => {
@@ -21,6 +25,18 @@ const SurveyDetailsPage = () => {
           dispatch({type : UPDATE_SURVEY_RESET})
         }
     }, [dispatch,deleteUpdateSurvey.isUpdated])
+
+    useEffect(() => {
+      dispatch(GetSurveyFavorites(auth.user._id));
+      if (addRemoveFavorite.isAdded) {
+        message.success(addRemoveFavorite.message)
+        dispatch({type : ADD_SURVEY_TO_FAVORITE_RESET})
+      }
+      if (addRemoveFavorite.isRemoved) {
+        message.success(addRemoveFavorite.message)
+        dispatch({type : REMOVE_SURVEY_FROM_FAVORITE_RESET})
+      }
+    }, [dispatch, auth,addRemoveFavorite.isAdded,addRemoveFavorite.isRemoved]);
 
   return (
     <SurveyDetailsLayout>
