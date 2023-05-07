9c5d86f41bae3dc010f80e53db003906416fde51
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-07 13:24:32 +0300

    added meta title to survey preview page

--- adminfrontend/src/pages/SurveyPreviewPage.js
+++ adminfrontend/src/pages/SurveyPreviewPage.js
@@ -14,19 +14,30 @@ import QuestionPreviewCard from "../components/card/QuestionPreviewCard";
 import SuccessResult from "../components/result/SuccessResult";
 import NotFoundResult from "../components/result/NotFoundResult";
 import { Form } from "antd";
+import MetaTitle from "../meta/MetaTitle";
+import { SUBMIT_SURVEY_RESET } from "../redux/constants/SurveyConstants";
 
 const SurveyPreviewPage = () => {
   const getSingleSurveyPreview = useSelector(
     (state) => state.survey.getSingleSurveyPreview
   );
   const getAllQuestion = useSelector((state) => state.question.getAllQuestion);
+  const createQuestion = useSelector((state) => state.question.createQuestion)
+  const submitSurvey = useSelector((state) => state.survey.submitSurvey)
   const { id } = useParams();
   const dispatch = useDispatch();
+
+
+  const handleGiveAnotherResponse  = () => {
+      dispatch({type : SUBMIT_SURVEY_RESET})
+  }
+
 
   useEffect(() => {
     dispatch(GetSingleSurveyPreview(id));
     dispatch(AllQuestionOfSurvey(id));
-  }, [dispatch,id]);
+  
+  }, [dispatch, id,submitSurvey.success]);
 
   const [responses, setResponses] = useState({});
 
@@ -37,16 +48,18 @@ const SurveyPreviewPage = () => {
     };
     dispatch(SubmitSurveyForm(data));
   };
-
-  const submitSurvey = useSelector((state) => state.survey.submitSurvey);
-
-
-
   return (
     <Fragment>
+        <MetaTitle title={`${getSingleSurveyPreview.survey.title} Anketi Önizleme`} name="önizleme" content="önzileme" />
+        <div className="d-flex justify-content-center">
+        <img 
+          width={'25%'}
+          src="https://www.akinsoft.com.tr/logo/images/akinsoft_yatay_logo/akinsoft_yatay_logo.png"
+        />
+        </div>
       <div className="container">
         {submitSurvey.success ? (
-          <SuccessResult />
+          <SuccessResult handleGiveAnotherResponse={handleGiveAnotherResponse}/>
         ) : getSingleSurveyPreview.loading ? (
           <LoadingSpinner />
         ) : getSingleSurveyPreview.success ? (
@@ -60,6 +73,7 @@ const SurveyPreviewPage = () => {
             ) : (
               getAllQuestion.data.questions.map((question) => (
                 <QuestionPreviewCard
+              
                   key={question._id}
                   question={question}
                   responses={responses}
@@ -74,8 +88,9 @@ const SurveyPreviewPage = () => {
               >
                 Gönder
               </button>
-              
+           
             </div>
+        
           </>
         ) : (
           <NotFoundResult />
